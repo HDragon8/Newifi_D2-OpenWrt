45623aa46a6b850bce627be0baec75643d0f2b05
HDragon <houyonghua2015@gmail.com>
2021-09-29 15:46:15 +0800

    helloword
    helloword

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,9 @@ rm -rf package/lean/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
 # Modify default hostname
 sed -i 's/OpenWrt/noodles/g' package/base-files/files/bin/config_generate
 
