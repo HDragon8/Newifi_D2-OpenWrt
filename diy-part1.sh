10919ce153aa1163702297a72b62d8448ba4453d
HDragon <houyonghua2015@gmail.com>
2021-10-02 08:08:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@ rm -rf package/lean/smartdns
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Modify default hostname
 sed -i 's/OpenWrt/noodles/g' package/base-files/files/bin/config_generate
